c20b751714e4182c5ab83d274263ded1f81f7029
OblivionSorcerer <aquariusstd@yandex.ru>
2023-03-12 11:18:22 +0400

    init

---
+++ PowerCollections.Tests/StackTests.cs
@@ -0,0 +1,144 @@
+﻿using Xunit;
+using Wintellect.PowerCollections;
+using System;
+
+
+namespace StackTests
+{
+    public class TestStack
+    {
+        //This test checks constructor
+        [Fact]
+        public void TestConstructor()
+        {
+            Stack<int> stackCards = new Stack<int>(3);
+            Assert.Equal(3, stackCards.capacity);
+        }
+        //These tests check IsEmpty 
+        [Fact]
+        public void IsEmptyShouldBeTrueWhenStackIsNew()
+        {
+            Stack<int> stackCards = new Stack<int>(5);
+            Assert.True(stackCards.IsEmpty);
+        }
+        [Fact]
+        public void IsEmptyShouldBeFalseAfterPush()
+        {
+            var stackCards = new Stack<int>(5);
+            stackCards.Push(1);
+            Assert.False(stackCards.IsEmpty);
+        }
+        [Fact]
+        public void IsEmptyShouldBeTrueAfterPushThenPop()
+        {
+            var stackCards = new Stack<int>(6);
+            stackCards.Push(5);
+            stackCards.Pop();
+            Assert.True(stackCards.IsEmpty);
+        }
+        //These tests check IsFull
+        [Fact]
+        public void IsFullShouldBeFalseWhenStackIsNew()
+        {
+            Stack<int> stackCards = new Stack<int>(5);
+            Assert.False(stackCards.IsFull);
+        }
+        [Fact]
+        public void PushWhenIsFullShouldBeTrue()
+        {
+            Stack<int> stackCard = new Stack<int>(2);
+            stackCard.Push(2);
+            stackCard.Push(2);
+            Action act = () => stackCard.Push(2);
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(act);
+            Assert.Equal("Переполнение стека!", exception.Message);
+        }
+        //This test checks Count
+        [Fact]
+        public void CountShouldBeZeroWhenStackIsNew()
+        {
+            Stack<int> stackCard = new Stack<int>(2);
+            Assert.Equal(0, stackCard.Count);
+        }
+        //These tests check Push
+        [Fact]
+        public void PushAndCountAfterPush()
+        {
+            Stack<int> stackCard = new Stack<int>(2);
+            stackCard.Push(43);
+            Assert.Equal(1, stackCard.Count);
+        }
+        [Fact]
+        public void PushWhenStackIsFull()
+        {
+            Stack<int> stackCard = new Stack<int>(2);
+            stackCard.Push(2);
+            stackCard.Push(2);
+            Action act = () => stackCard.Push(2);
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(act);
+            Assert.Equal("Переполнение стека!", exception.Message);
+        }
+        //These tests check Top()
+        [Fact]
+        public void TopShouldBeTheSameAsLastPushedElement()
+        {
+            Stack<string> stackCard = new Stack<string>(2);
+            stackCard.Push("Hello");
+            Assert.Equal("Hello", stackCard.Top());
+        }
+        [Fact]
+        public void TopShouldBeThePreviousPushedElementAfterPop()
+        {
+            Stack<string> stackCard = new Stack<string>(2);
+            stackCard.Push("Hello");
+            stackCard.Push(" world!");
+            stackCard.Pop();
+            Assert.Equal("Hello", stackCard.Top());
+        }
+        [Fact]
+        public void TopWhenStackIsEmpty()
+        {
+            Stack<int> stackCard = new Stack<int>(2);
+            Action act = () => stackCard.Top();
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(act);
+            Assert.Equal("Стек пуст!", exception.Message);
+        }
+        //These tests check Pop()
+        [Fact]
+        public void PoppedElementShouldBeAsLastPushedElement()
+        {
+            Stack<int> stackCard = new Stack<int>(2);
+            stackCard.Push(2);
+            stackCard.Push(32);
+            Assert.Equal(32, stackCard.Pop());
+            Assert.Equal(2, stackCard.Top());
+        }
+        [Fact]
+        public void PopWhenStackIsEmpty()
+        {
+            Stack<int> stackCard = new Stack<int>(2);
+            Action act = () => stackCard.Pop();
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(act);
+            Assert.Equal("Стек пуст!", exception.Message);
+        }
+        //Test Foreach
+        [Fact]
+        public void ForEachShouldBeAbleEnumerateElementsFromTopToBottom()
+        {
+            int counter = 3;
+            Stack<int> stackCard = new Stack<int>(counter);
+            int[] testArr = new int[counter];
+            int[] trueArr = new int[] { 323, 32, 2 };
+            stackCard.Push(2);
+            stackCard.Push(32);
+            stackCard.Push(323);
+            int i = 0;
+            foreach (int item in stackCard)
+            {
+                testArr[i] = item;
+                i++;
+            }
+            Assert.Equal(testArr, trueArr);
+        }
+    }
+}
---
+++ PowerCollections/Stack.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Collections;
+
+namespace Wintellect.PowerCollections
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private T[] stack;
+        private int position = -1;
+        public readonly int capacity = 0;
+        public Stack(int capacityOfStack)
+        {
+            if(capacityOfStack < 0)
+            {
+                capacityOfStack = 0;
+            }
+            capacity = capacityOfStack;
+            stack = new T[capacity];
+        }
+        public Stack()
+        {
+            capacity = 100;
+            stack = new T[capacity];
+        }
+        public bool IsEmpty
+        {
+            get { return position < 0; }
+        }
+        public int Count
+        {
+            get { return position+1; }
+        }
+        public bool IsFull
+        {
+            get { return Count == capacity; }
+        }
+        public void Push(T element)
+        {
+            if(IsFull)
+                throw new InvalidOperationException("Переполнение стека!");
+            stack[++position] = element;
+        }
+        public T Pop()
+        {
+            if(IsEmpty)
+                throw new InvalidOperationException("Стек пуст!");
+            T currentElement = stack[position];
+            stack[position] = default(T)!;
+            position--;
+            return currentElement;
+        }
+        public T Top()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст!");
+            return stack[position];
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (IEnumerator<T>)GetEnumerator();
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new StackEnum<T>(stack, Count);
+        }
+    }
+    public class StackEnum<T> : IEnumerator<T>
+    {
+        private T[] arr;
+        private int position;
+        private int capacity;
+        public StackEnum(T[] arr, int position)
+        {
+            this.arr = arr;
+            this.position = position;
+            capacity = position;
+        }
+        public T Current { get; private set; }
+
+        object IEnumerator.Current => this.arr[position]!;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            position--;
+            return position > -1;
+        }
+
+        public void Reset()
+        {
+            position = capacity;
+        }
+    }
+}
